12ad41a2ee21d3e6794309b5ec0b389f6ac99190
Abbey Hart <abbeyhrt@gmail.com>
2017-12-03 18:26:49 -0600

    Videos 10-14

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { format } from 'url';
+import PropTypes from 'prop-types';
+import { getFunName } from '../helpers';
 
 //remember that you can't put two parent elements into a react component
-
+//stay away from touching the DOM in React
+//use a ref instead
+//constructore
 class StorePicker extends React.Component {
+  //This is one way to bind the Store Picker to a method using constructors
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    //first grab the text from the box
+    // const value = $('input').val();
+    console.log(this.storeInput.value);
+    //second we are goin to transition to /store/storeid
+    const storeId = this.storeInput.value;
+
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
   render() {
-    return <form className="store-selector" />;
+    return (
+      <form className="store-selector" onSubmit={e => this.goToStore(e)}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={input => {
+            this.storeInput = input;
+          }}
+        />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
   }
 }
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object,
+};
 
 export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
+import './css/style.css';
+import App from './components/App';
 import StorePicker from './components/StorePicker';
-
+import NotFound from './components/NotFound';
 //create a component
 //render(<component you created/>, what dom element it should render out to)
-render(<StorePicker />, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+render(<Root />, document.querySelector('#main'));
